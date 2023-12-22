e5b9a30bbf99da60ff71fd5e74cca046cbc85c1e
Min-Hung-Hsieh <hsiinung@gmail.com>
2023-12-22 13:03:11 -0500

    Practice

---
+++ LoginGUI/src/LoginApp.java
@@ -0,0 +1,14 @@
+
+/**
+ *
+ * @author Milo Hsieh
+ */
+
+public class LoginApp {
+    
+    public static void main(String args[]) {
+        LoginGUI loginFrame = new LoginGUI();
+        loginFrame.setVisible(true);
+        
+    }
+}
